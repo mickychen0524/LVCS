2ecf20f72940674478e037578369808758220976
Swayam Agrawal <agrawal.swayam@gmail.com>
2017-09-06 00:23:56 +0530

    Version 8 min target

--- WifiClientChat/WifiClientChat/WebrtcManager.swift
+++ WifiClientChat/WifiClientChat/WebrtcManager.swift
@@ -84,7 +84,14 @@ class WebrtcManager: NSObject,RTCPeerConnectionDelegate,RTCSessionDescriptionDel
         let mandatoryConstrains:NSArray = [RTCPair.init(key: "maxFrameRate", value: "35")]
         _ = RTCMediaConstraints.init(mandatoryConstraints: mandatoryConstrains as! [Any], optionalConstraints: nil)
         
-        let cameraID = AVCaptureDevice.defaultDevice(withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.front)?.localizedName
+        var cameraID: String?
+        for captureDevice in AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo) {
+            // Support Front cam alone, as it's suitable for video conferencing
+            if (captureDevice as AnyObject).position == AVCaptureDevicePosition.front {
+                cameraID = (captureDevice as AnyObject).localizedName
+            }
+        }
+        
         let videoCapturer = RTCVideoCapturer(deviceName: cameraID)
         self.videoCapturer = videoCapturer
         let videoSource = peerConnectionFactory?.videoSource(with: videoCapturer, constraints: nil)
@@ -95,7 +102,13 @@ class WebrtcManager: NSObject,RTCPeerConnectionDelegate,RTCSessionDescriptionDel
     
     func getBackVideo() -> RTCVideoTrack
     {
-        let cameraID = AVCaptureDevice.defaultDevice(withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.back)?.localizedName
+        var cameraID: String?
+        for captureDevice in AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo) {
+            // Support Front cam alone, as it's suitable for video conferencing
+            if (captureDevice as AnyObject).position == AVCaptureDevicePosition.back {
+                cameraID = (captureDevice as AnyObject).localizedName
+            }
+        }
         let videoCapturer = RTCVideoCapturer(deviceName: cameraID)
         self.videoCapturer = videoCapturer
         let videoSource = peerConnectionFactory?.videoSource(with: videoCapturer, constraints: nil)
